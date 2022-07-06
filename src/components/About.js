28e0a511c0f477f6f9e22691a07fb3dc7aa8ee42
Negar Eghbali <negar.en@gmail.com>
2022-07-06 18:39:47 +0200

    first commit

---
+++ src/components/About.js
@@ -0,0 +1,44 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <section id="about">
+      <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
+        <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+          <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
+            Hi, I am Negar
+            <br className="hidden lg:inline-block" />
+            A passionate creative full stack front-end developer
+          </h1>
+          <p className="mb-8 leading-relaxed">
+            I have decided to follow up my passion and have applied for Hack Your Future web development educational program and joined HYF familly on September 2021.
+            Every single moment of this journey was alog leaning new technologies, materials and team work process.
+            I beleive that using the benefit of my past experience in project management beside web development knowledge, I can increase the pace of problem solving within web development projects.
+            I am driven by finding practical, stable solutions for issues.
+          </p>
+          <div className="flex justify-center">
+            <a
+              href="#contact"
+              className="inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg"
+            >
+              Contact me
+            </a>
+            <a
+              href="#projects"
+              className="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg"
+            >
+              Projects
+            </a>
+          </div>
+        </div>
+        <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+          <img
+            className="object-cover object-center rounded"
+            alt="hero"
+            src="./profilePic.png"
+          />
+        </div>
+      </div>
+    </section>
+  );
+};
---
+++ src/components/Data.js
@@ -0,0 +1,86 @@
+import mealsharing from "../images/mealsharing.png";
+ import weatherApp from "../images/weatherApp.png";
+ import whoIsTheFastest from "../images/whoIsTheFastest.png";
+ import simplySpices from "../images/hero_image.png"
+
+export const projects = [
+  {
+    title: "Weather App",
+    subtitle: "Using HTML, CSS, JS",
+    description: "In this App you can check the weather base on the location",
+    image: weatherApp,
+    link: "https://github.com/negaren/hyf-homework/blob/master/javascript/javascript3/week1/weatherapp.js",
+  },
+
+  {
+    title: "meal sharing",
+    subtitle: "Using ReactJs",
+    description:
+      "An application to let people share their food with eachother and enjoy new tastes.",
+    image: mealsharing,
+    link: "https://meal-sharing-negar.herokuapp.com/",
+  },
+  {
+    title: "Game, Who's the fastest",
+    subtitle: "Using HTML, CSS, JS",
+    description: "Who is the is the fastest on clicking.",
+    image: whoIsTheFastest,
+    link: "https://github.com/negaren/hyf-homework/blob/master/javascript/javascript2/week3/warmup.js",
+  },
+  {
+    title: "Todo app",
+    subtitle: "Using ReactJs",
+    description: "Have your daily schedule in your phone ",
+    image: "",
+    link: "https://github.com/negaren/hyf-homework/tree/master/react/react1/week2/my-app/src",
+  },
+  {
+    title: "Simply Spices Webshop",
+    subtitle: "Using ReactJs, Node.js, HTML, CSS, Knex, Swagger",
+    description: "This website is presenting product to be purchased.",
+    image: simplySpices,
+    link: "https://staging-webshop-class20-fp.herokuapp.com/",
+  },
+];
+export const skills = [
+  "JavaScript",
+  "ReactJS",
+  "HTML",
+  "CSS",
+  "NodeJS",
+  "ExpressJS",
+  "MongoDB",
+  "KNEX",
+  "MySQL",
+  "Git",
+  "RESTApi",
+  "Docker",
+];
+export const pictures = [{ picture1: "png" }];
+export const educations = [
+  {
+    title: "HackYourFuture, Web Development ",
+    place: "Copenhagen, Denmark",
+    period: "May 2021 _ March 2022",
+  },
+  {
+    title: "Graduated with Bachelor's Degree",
+    place: "Azad University, B.A. in Judicial Law Studies",
+    period: "2005 _ 2010",
+  },
+];
+export const experience = [
+  {
+    period: "July 2020 - present",
+    position: "Sale Assistance",
+    company: "ZARA, Field's, Copenhagen",
+    details: "Customer Service",
+  },
+  {
+    period: "September 2019 _ Jun 2020",
+    position: "Sale Assistance",
+    company: "Plantorama",
+    details:
+      "Consultancy in the choice of plants and plant design for indoor and outdoor plants",
+  },
+];
